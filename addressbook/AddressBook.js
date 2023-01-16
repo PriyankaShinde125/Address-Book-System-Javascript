c3c3c8b9f6502526c53e3e92014feebf3154eb47
PriyankaShinde125 <pkshinde125@gmail.com>
2023-01-16 23:31:35 +0530

    ADDED : Code to validate contact details

--- addressbook/AddressBook.js
+++ addressbook/AddressBook.js
@@ -12,6 +12,71 @@ class Contact {
         this.zip = zip;
     }
 
+    get getFirstName() {
+        return this.firstName;
+    }
+
+
+    set setFirstName(firstName) {
+        this.firstName = firstName;
+    }
+
+    get getLastName() {
+        return this.lastName;
+    }
+
+    set setLastName(lastName) {
+        this.lastName = lastName;
+    }
+
+    get getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    set setPhoneNumber(phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    get getEmailId() {
+        return this.emailId;
+    }
+
+    set setEmailId(emailId) {
+        this.emailId = this.emailId;
+    }
+
+    get getArea() {
+        return this.area;
+    }
+
+    set setArea(area) {
+        this.area = area;
+    }
+
+    get getCity() {
+        return this.city;
+    }
+
+    set setCity(city) {
+        this.city = city;
+    }
+
+    get getState() {
+        return this.state;
+    }
+
+    set setState(state) {
+        this.state = state;
+    }
+
+    get getZip() {
+        return this.zip;
+    }
+
+    set setZip(zip) {
+        this.zip = zip;
+    }
+
     toString() {
         return '{ First name = ' + this.firstName + '  Lastname = ' + this.lastName + '  Phone number = ' + this.phoneNumber + ' Email Id = ' + this.emailId + '  Address = ' + this.area + ' , ' + this.city + ' , ' + this.state + ' , ' + this.zip + '}';
     }
@@ -31,7 +96,52 @@ function createContact() {
     return contact;
 }
 
+function isValidContact(contact) {
+    let nameRegex = new RegExp(/^[A-Z]{1}[a-z]{2,}/);
+    let phoneNumberRegex = new RegExp(/^(?!.*0{4})\+?([1][\s\-])?[1-9]{1,3}\s[0-9]{10}$/);
+    let emailIdRegex = new RegExp(/^((?!.*[\.\-_]{2,})^[^0-9\.\-][a-z0-9\-\.+_]*[^.+_])*@+([a-z0-9]{1,6})+(\.[a-z]{2,6}){1,2}?$/);
+    let areaCityStateRegex = new RegExp(/^[a-z]{3,}$/);
+    let zipRegex = new RegExp(/^(?!(0))[0-9]{6}$/);
+    console.log(contact.getFirstName);
+    if (!nameRegex.test(contact.getFirstName)) {
+        console.log("Invalid first name");
+        return false;
+    }
+    if (!nameRegex.test(contact.getLastName)) {
+        console.log("Invalid last name");
+        return false;
+    }
+    if (!phoneNumberRegex.test(contact.getPhoneNumber)) {
+        console.log("Invalid phone number");
+        return false;
+    }
+    if (!emailIdRegex.test(contact.getEmailId)) {
+        console.log("Inalid email id");
+        return false;
+    }
+    if (!areaCityStateRegex.test(contact.getArea)) {
+        console.log("Invalid area");
+        return false;
+    }
+    if (!areaCityStateRegex.test(contact.getCity)) {
+        console.log("Invalid city");
+        return false;
+    }
+    if (!areaCityStateRegex.test(contact.getState)) {
+        console.log("Invalid state");
+        return false;
+    }
+    if(!zipRegex.test(contact.getZip)){
+        console.log("Invalid zip");
+        return false;
+    }
+    return true;
+}
+
 let contact = createContact();
-addressBook.push(contact);
+let isValid = isValidContact(contact);
+if(isValid){
+    addressBook.push(contact);
+}
 console.log(addressBook);
 
